935076113d709745dbacec971bfb304ce4c9e9e7
Kir Shatrov <shatrov@me.com>
2017-01-23 13:42:33 -0500

    Initial test suite

--- test/kubernetes_deploy_test.rb
+++ test/kubernetes_deploy_test.rb
@@ -1,11 +1,57 @@
 require 'test_helper'
 
 class KubernetesDeployTest < Minitest::Test
+  def setup
+    @logger_stream = StringIO.new
+    @logger = Logger.new(@logger_stream)
+    KubernetesDeploy.logger = @logger
+  end
+
+  def teardown
+    @logger_stream.close
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::KubernetesDeploy::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_basic
+    runner = KubernetesDeploy::Runner.new(
+      namespace: "trashbin",
+      current_sha: "abcabcabc",
+      context: "minikube",
+      template_dir: File.expand_path("./test/fixtures/trashbin"),
+      wait_for_completion: true,
+    )
+    runner.run
+
+    pods = kubeclient.get_pods
+    assert_equal 3, pods.size
+
+    speaker_pods, rest_pods = pods.partition { |p| p.metadata.name =~ /speaker-pod/ }
+    assert_equal 2, rest_pods.size
+    assert_equal 1, speaker_pods.size
+
+    statuses = rest_pods.map { |pod| pod.status.phase }
+    assert statuses.all? { |p| p == "Running" }, "Pod statuses have to be Running, got: #{statuses.inspect}"
+  end
+
+  def test_initializer
+    runner = KubernetesDeploy::Runner.new(
+      namespace: "",
+      current_sha: "",
+      context: "",
+      template_dir: "unknown",
+      wait_for_completion: true,
+    )
+    error = assert_raises(KubernetesDeploy::FatalDeploymentError) do
+      runner.run
+    end
+
+    assert_includes error.to_s, "Kube config not found at /home/"
+    assert_includes error.to_s, "Current SHA must be specified"
+    assert_includes error.to_s, "Namespace must be specified"
+    assert_includes error.to_s, "Context must be specified"
+    assert_match /Template path (\S+) doesn't exist/, error.to_s
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,64 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'kubernetes-deploy'
+require 'kubeclient'
+require 'pry'
 
 require 'minitest/autorun'
+
+ENV["KUBECONFIG"] ||= "#{Dir.home}/.kube/config"
+
+module KubeclientHelpers
+  def kubeclient
+    @kubeclient ||= begin
+      config = Kubeclient::Config.read(ENV["KUBECONFIG"])
+      unless config.contexts.include?("minikube")
+        puts "`minikube` context should be configured in your KUBECONFIG (#{ENV["KUBECONFIG"]})"
+      end
+
+      client = Kubeclient::Client.new(
+        config.context.api_endpoint,
+        config.context.api_version,
+        {
+          ssl_options: config.context.ssl_options,
+          auth_options: config.context.auth_options
+        }
+      )
+      client.discover
+      client
+    end
+  end
+end
+
+Minitest::Test.include(KubeclientHelpers)
+
+module TestProvisioner
+  extend KubeclientHelpers
+
+  def self.prepare_namespace(namespace)
+    begin
+      kubeclient.get_namespace(namespace)
+    rescue KubeException
+      ns = Kubeclient::Namespace.new
+      ns.metadata = { name: namespace }
+      kubeclient.create_namespace(ns)
+    end
+  end
+
+  def self.prepare_pv(name)
+    begin
+      kubeclient.get_persistent_volume(name)
+    rescue KubeException
+      pv = Kubeclient::PersistentVolume.new
+      pv.metadata = { name: name }
+      pv.spec = {
+        accessModes: ["ReadWriteOnce"],
+        capacity: { storage: "1Gi" },
+        hostPath: { path: "/data/#{name}" }
+      }
+      kubeclient.create_persistent_volume(pv)
+    end
+  end
+end
+
+TestProvisioner.prepare_namespace("trashbin")
+TestProvisioner.prepare_pv("pv0001")
