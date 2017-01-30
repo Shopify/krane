00b3787cc98c12d00d5878228f6bb0732f30d7c7
Will Farrington <wcfarrington@gmail.com>
2017-01-30 13:41:22 -0500

    Add some deploy_succeeded? logic to Cloudsql

--- lib/kubernetes-deploy/kubernetes_resource/cloudsql.rb
+++ lib/kubernetes-deploy/kubernetes_resource/cloudsql.rb
@@ -10,15 +10,18 @@ module KubernetesDeploy
       _, st = run_kubectl("get", type, @name)
       @found = st.success?
       @status = true
+
+
+
       log_status
     end
 
     def deploy_succeeded?
-      exists?
+      cloudsql_proxy_deployment_exists? && mysql_service_exists?
     end
 
     def deploy_failed?
-      false
+      !cloudsql_proxy_deployment_exists? || !mysql_service_exists?
     end
 
     def tpr?
@@ -28,5 +31,36 @@ module KubernetesDeploy
     def exists?
       @found
     end
+
+    private
+    def cloudsql_proxy_deployment_exists?
+      deployments, st = run_kubectl("get", "deployments", "--selector", "name=cloudsql-proxy", "--namespace=#{@namespace}", "-o=json")
+      if st.success?
+        deployment_list = JSON.parse(deployments)
+        cloudsql_proxy = deployment_list["items"].first
+
+        if cloudsql_proxy.fetch("status", {}).fetch("availableReplicas", -1) == cloudsql_proxy["replicas"]
+          # all cloudsql-proxy pods are running
+          return true
+        end
+      end
+
+      false
+    end
+
+    def mysql_service_exists?
+      services, st = run_kubectl("get", "services", "--selector", "name=mysql", "--namespace=#{@namespace}", "-o=json")
+      if st.success?
+        services_list = JSON.parse(services)
+
+        if .any? { |s| s.fetch("spec", {}).fetch("clusterIP", "") != "" }
+          # the service has an assigned cluster IP and is therefore functioning
+          return true
+        end
+      end
+
+      false
+    end
+
   end
 end
