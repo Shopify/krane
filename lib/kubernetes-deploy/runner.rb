a4bca98de970156fc9ad094443e46f2262a9a8ee
Katrina Verey <kn.verey@gmail.com>
2017-04-11 13:40:54 -0400

    Adjust expected apply error message for v1.6

--- lib/kubernetes-deploy/runner.rb
+++ lib/kubernetes-deploy/runner.rb
@@ -149,8 +149,8 @@ MSG
     # to make it easier for developer to understand what's going on
     def inspect_kubectl_out_for_files(stderr)
       # Output example:
-      # error: unable to decode "/tmp/path/to/file": [pos 96]: json: expect char '"' but got char '{'
-      match = stderr.match(/error: unable to decode "(?<path>\S+)":/)
+      # Error from server (BadRequest): error when creating \"/var/folders/n9/9x7qbcsd4tv2t3xkz3mwsb800000gp/T/configmap-data20170411-33615-gqq5oh.yml20170411-33615-t0t3m\":
+      match = stderr.match(/BadRequest.*"(?<path>\/\S+\.yml\S+)"/)
       return unless match
 
       path = match[:path]
