84b647fcc80b94499b37f1e538c859b04333706c
Katrina Verey <kn.verey@gmail.com>
2017-07-12 20:06:17 -0400

    Add Simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
+if ENV["COVERAGE"]
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'test/'
+  end
+
+  if ENV["CODECOV_TOKEN"]
+    require 'codecov'
+    SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'kubernetes-deploy'
 require 'kubeclient'
