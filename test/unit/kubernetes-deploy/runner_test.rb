8815764a2896fa77aa230121695e5439ce382f0a
Daniel Turner <daniel.turner@shopify.com>
2017-09-26 11:44:00 -0500

    Add a test

--- test/unit/kubernetes-deploy/runner_test.rb
+++ test/unit/kubernetes-deploy/runner_test.rb
@@ -6,11 +6,33 @@ class RunnerTest < KubernetesDeploy::TestCase
     refute_nil ::KubernetesDeploy::VERSION
   end
 
+  def test_error_message_when_kubeconfig_not_set
+    runner_with_env(nil)
+    assert_logs_match("Configuration invalid")
+    assert_logs_match("$KUBECONFIG not set")
+    assert_logs_match("Current SHA must be specified")
+    assert_logs_match("Namespace must be specified")
+    assert_logs_match("Context must be specified")
+    assert_logs_match(/Template directory (\S+) doesn't exist/)
+  end
+
   def test_initializer
+    runner_with_env("/this-really-should/not-exist")
+    assert_logs_match("Configuration invalid")
+    assert_logs_match("Kube config not found at /this-really-should/not-exist")
+    assert_logs_match("Current SHA must be specified")
+    assert_logs_match("Namespace must be specified")
+    assert_logs_match("Context must be specified")
+    assert_logs_match(/Template directory (\S+) doesn't exist/)
+  end
+
+  private
+
+  def runner_with_env(value)
     # TODO: Switch to --kubeconfig for kubectl shell out and pass env var as arg to Runner init
     # Then fix this crappy env manipulation
     original_env = ENV["KUBECONFIG"]
-    ENV["KUBECONFIG"] = "/this-really-should/not-exist"
+    ENV["KUBECONFIG"] = value
 
     runner = KubernetesDeploy::Runner.new(
       namespace: "",
@@ -20,14 +42,6 @@ class RunnerTest < KubernetesDeploy::TestCase
       template_dir: "unknown",
     )
     runner.run
-
-    assert_logs_match("Configuration invalid")
-    assert_logs_match("Kube config not found at /this-really-should/not-exist")
-    assert_logs_match("Current SHA must be specified")
-    assert_logs_match("Namespace must be specified")
-    assert_logs_match("Context must be specified")
-    assert_logs_match(/Template directory (\S+) doesn't exist/)
-
   ensure
     ENV["KUBECONFIG"] = original_env
   end
