2bfd73e3b43dd10d6ba09285c749986e51c5c858
Devon Boyer <devonboyer@users.noreply.github.com>
2017-10-19 18:21:18 -0400

    Change Memcached to verify ConfigMap exists instead of Secret. (#187)
    * change secret to config map
    
    * Version bump

--- lib/kubernetes-deploy/kubernetes_resource/memcached.rb
+++ lib/kubernetes-deploy/kubernetes_resource/memcached.rb
@@ -2,16 +2,16 @@
 module KubernetesDeploy
   class Memcached < KubernetesResource
     TIMEOUT = 5.minutes
-    SECRET_NAME = "memcached-url"
+    CONFIGMAP_NAME = "memcached-url"
 
     def sync
       _, _err, st = kubectl.run("get", type, @name)
       @found = st.success?
       @deployment_exists = memcached_deployment_exists?
       @service_exists = memcached_service_exists?
-      @secret_exists = memcached_secret_exists?
+      @configmap_exists = memcached_configmap_exists?
 
-      @status = if @deployment_exists && @service_exists && @secret_exists
+      @status = if @deployment_exists && @service_exists && @configmap_exists
         "Provisioned"
       else
         "Unknown"
@@ -19,7 +19,7 @@ module KubernetesDeploy
     end
 
     def deploy_succeeded?
-      @deployment_exists && @service_exists && @secret_exists
+      @deployment_exists && @service_exists && @configmap_exists
     end
 
     def deploy_failed?
@@ -33,26 +33,22 @@ module KubernetesDeploy
     private
 
     def memcached_deployment_exists?
-      deployments, _err, st = kubectl.run("get", "deployments", "-l name=#{@name}", "-o=json")
+      deployment, _err, st = kubectl.run("get", "deployments", "memcached-#{@name}", "-o=json")
       return false unless st.success?
-      parsed = JSON.parse(deployments)
-      return false if parsed.fetch("items", []).count == 0
-      deployment = parsed.fetch("items", []).first
-      status = deployment.fetch("status", {})
+      parsed = JSON.parse(deployment)
+      status = parsed.fetch("status", {})
       status.fetch("availableReplicas", -1) == status.fetch("replicas", 0)
     end
 
     def memcached_service_exists?
-      services, _err, st = kubectl.run("get", "services", "-l name=#{@name}", "-o=json")
+      service, _err, st = kubectl.run("get", "services", "memcached-#{@name}", "-o=json")
       return false unless st.success?
-      parsed = JSON.parse(services)
-      return false if parsed.fetch("items", []).count == 0
-      service = parsed.fetch("items", []).first
-      service.dig("spec", "clusterIP").present?
+      parsed = JSON.parse(service)
+      parsed.dig("spec", "clusterIP").present?
     end
 
-    def memcached_secret_exists?
-      secret, _err, st = kubectl.run("get", "secrets", SECRET_NAME, "-o=json")
+    def memcached_configmap_exists?
+      secret, _err, st = kubectl.run("get", "configmaps", CONFIGMAP_NAME, "-o=json")
       return false unless st.success?
       parsed = JSON.parse(secret)
       parsed.dig("data", @name).present?
