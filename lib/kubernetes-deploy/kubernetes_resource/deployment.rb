2095ea7d3576effd7764929d94f0360968788a61
Stefan Budeanu <stefanmb@users.noreply.github.com>
2017-10-27 14:00:03 -0400

    Rubocop

--- lib/kubernetes-deploy/kubernetes_resource/deployment.rb
+++ lib/kubernetes-deploy/kubernetes_resource/deployment.rb
@@ -54,7 +54,7 @@ module KubernetesDeploy
     def timeout_message
       progress_seconds = @definition['spec']['progressDeadlineSeconds']
       if progress_seconds
-        "Deploy timed out due to progressDeadlineSeconds of #{progress_seconds} seconds, reason: #{@progress["reason"]} #{@latest_rs&.timeout_message}"
+        "Deploy timed out due to progressDeadlineSeconds of #{progress_seconds} seconds, reason: #{@progress['reason']} #{@latest_rs&.timeout_message}"
       else
         @latest_rs&.timeout_message
       end
