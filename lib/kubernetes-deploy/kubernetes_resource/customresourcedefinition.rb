permaninder/kubernetes-deploy

Runtime resource discovery

lib/kubernetes-deploy/kubernetes_resource/customresourcedefinition.rb
@@ -1,0 +1,6 @@
+# frozen_string_literal: true
+module KubernetesDeploy
+  class CustomResourceDefinition < GenericResource
+    TIMEOUT = 10.seconds
+  end
+end

Shopify/krane

lib/kubernetes-deploy/kubernetes_resource/customresourcedefinition.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 module KubernetesDeploy
-  class ServiceAccount < KubernetesResource
-    TIMEOUT = 30.seconds
+  class CustomResourceDefinition < KubernetesResource
+    TIMEOUT = 10.seconds
     PREDEPLOY = true
-
-    def status
-      exists? ? "Created" : "Unknown"
-    end
 
     def deploy_succeeded?
       exists?