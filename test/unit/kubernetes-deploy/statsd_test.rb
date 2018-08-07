dcf3dcc8659d79efcc2563743abb9cd0dc1d7d0c
Edouard CHIN <edouard.chin@shopify.com>
2018-08-07 15:47:44 -0400

    Check for presence of the `STATSD_IMPLEMENTATION' env:
    - Calling `empty?` when ENV['STATSD_IMPLEMENTATION'] isn't set makes kubernetes-deploy to crash.
    
      Fixing the issue by calling ActiveSuport's `present?` instead

---
+++ test/unit/kubernetes-deploy/statsd_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class StatsDTest < KubernetesDeploy::TestCase
+  def test_build_when_statsd_addr_env_present_but_statsd_implementation_is_not
+    original_addr = ENV['STATSD_ADDR']
+    ENV['STATSD_ADDR'] = '127.0.0.1'
+    original_impl = ENV['STATSD_IMPLEMENTATION']
+    ENV['STATSD_IMPLEMENTATION'] = nil
+    original_dev = ENV['STATSD_DEV']
+    ENV['STATSD_DEV'] = nil
+
+    KubernetesDeploy::StatsD.build
+
+    assert_equal :datadog, StatsD.backend.implementation
+  ensure
+    ENV['STATSD_ADDR'] = original_addr
+    ENV['STATSD_IMPLEMENTATION'] = original_impl
+    ENV['STATSD_DEV'] = original_dev
+  end
+end
