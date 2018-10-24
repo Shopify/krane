e5a5fd8f476b88fd0ee5828e22b60283412393bd
Daniel Turner <daniel.turner@shopify.com>
2018-10-24 11:03:23 -0700

    Add tests

--- lib/kubernetes-deploy/kubernetes_resource/job.rb
+++ lib/kubernetes-deploy/kubernetes_resource/job.rb
@@ -12,10 +12,8 @@ module KubernetesDeploy
 
     def deploy_failed?
       return false unless deploy_started?
+      return true if failed_status_condition
       return false unless @instance_data.dig("spec", "backoffLimit").present?
-      (@instance_data.dig("status", "conditions") || []).each do |condition|
-        return true if condition["type"] == 'Failed' && condition['status'] == "True"
-      end
       (@instance_data.dig("status", "failed") || 0) >= @instance_data.dig("spec", "backoffLimit")
     end
 
@@ -33,7 +31,19 @@ module KubernetesDeploy
       end
     end
 
+    def failure_message
+      if (condition = failed_status_condition.presence)
+        "#{condition['reason']} (#{condition['message']})"
+      end
+    end
+
     private
+
+    def failed_status_condition
+      (@instance_data.dig("status", "conditions") || []).detect do |condition|
+        condition["type"] == 'Failed' && condition['status'] == "True"
+      end
+    end
 
     def done?
       (@instance_data.dig("status", "succeeded") || 0) == @instance_data.dig("spec", "completions")
---
+++ test/unit/kubernetes-deploy/kubernetes_resource/job_test.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+class JobTest < KubernetesDeploy::TestCase
+  def test_job_fails_with_failed_status_condition
+    status = {
+      status: {
+        conditions: [{
+          lastProbeTime: "2018-10-12T19:49:33Z",
+          lastTransitionTime: "2018-10-12T19:49:33",
+          message: "Job has reached the specified backoff limit",
+          reason: "BackoffLimitExceeded",
+          status: "True",
+          type: "Failed",
+        }],
+        failed: 2,
+        startTime: "2018-10-12T19:49:28Z",
+      }
+    }
+    job = build_synced_job(job_spec.merge(status).deep_stringify_keys)
+
+    assert_predicate job, :deploy_failed?
+
+    expected_msg = "BackoffLimitExceeded (Job has reached the specified backoff limit)"
+    assert_equal expected_msg, job.failure_message
+  end
+
+  def test_job_fails_without_failed_status_condition
+    status = {
+      spec: {
+        backoffLimit: 1
+      },
+      status: {
+        failed: 2,
+        startTime: "2018-10-12T19:49:28Z",
+      }
+    }
+    job = build_synced_job(job_spec.merge(status).deep_stringify_keys)
+
+    assert_predicate job, :deploy_failed?
+  end
+
+  private
+
+  def job_spec
+    { metadata: { name: 'test-job' } }
+  end
+
+  def build_synced_job(template)
+    job = KubernetesDeploy::Job.new(namespace: 'test', context: 'nope', definition: template,
+      logger: @logger)
+    job.deploy_started_at = Time.now.utc
+    mediator = KubernetesDeploy::SyncMediator.new(namespace: 'test', context: 'minikube', logger: logger)
+    mediator.expects(:get_instance).with('Job', anything).returns(template)
+    job.sync(mediator)
+    job
+  end
+end
