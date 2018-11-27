042dd727d1dc31baa1adfb10aea92671d75f1c1e
timothysmith0609 <tim.smith@shopify.com>
2018-11-27 09:08:03 -0500

    policial

--- test/helpers/statsd_helper.rb
+++ test/helpers/statsd_helper.rb
@@ -1,13 +1,17 @@
+# frozen_string_literal: true
 module StatsDHelper
   extend self
 
   def capture_statsd_calls(&block)
     mock_backend = ::StatsD::Instrument::Backends::CaptureBackend.new
-    old_backend, KubernetesDeploy::StatsD.backend = KubernetesDeploy::StatsD.backend, mock_backend
-    block.call
+    old_backend = KubernetesDeploy::StatsD.backend
+    KubernetesDeploy::StatsD.backend = mock_backend
+
+    yield
+
     mock_backend.collected_metrics
   ensure
-    if old_backend.kind_of?(::StatsD::Instrument::Backends::CaptureBackend)
+    if old_backend.is_a?(::StatsD::Instrument::Backends::CaptureBackend)
       old_backend.collected_metrics.concat(mock_backend.collected_metrics)
     end
     KubernetesDeploy::StatsD.backend = old_backend
