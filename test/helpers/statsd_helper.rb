cfa0080e323fe8b04a8af8fd8e330a1ea97dff37
timothysmith0609 <tim.smith@shopify.com>
2018-11-27 08:04:37 -0500

    doi, remove pry

--- test/helpers/statsd_helper.rb
+++ test/helpers/statsd_helper.rb
@@ -1,8 +1,7 @@
 module StatsDHelper
   extend self
-  
+
   def capture_statsd_calls(&block)
-    binding.pry
     mock_backend = ::StatsD::Instrument::Backends::CaptureBackend.new
     old_backend, KubernetesDeploy::StatsD.backend = KubernetesDeploy::StatsD.backend, mock_backend
     block.call
@@ -11,7 +10,6 @@ module StatsDHelper
     if old_backend.kind_of?(::StatsD::Instrument::Backends::CaptureBackend)
       old_backend.collected_metrics.concat(mock_backend.collected_metrics)
     end
-
     KubernetesDeploy::StatsD.backend = old_backend
   end
-end  
+end
