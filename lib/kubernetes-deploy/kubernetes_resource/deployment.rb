62c071c2683bf896d7505a0bb653fca08f4532a2
Timothy Smith <admins@shopify.com>
2019-01-15 23:45:27 +0100

    Automatic corrections for lib/kubernetes-deploy/kubernetes_resource/deployment.rb

--- lib/kubernetes-deploy/kubernetes_resource/deployment.rb
+++ lib/kubernetes-deploy/kubernetes_resource/deployment.rb
@@ -81,7 +81,7 @@ module KubernetesDeploy
     def deploy_timed_out?
       return false if deploy_failed?
       return super if timeout_override
-      
+
       # Do not use the hard timeout if progress deadline is set
       progress_condition.present? ? deploy_failing_to_progress? : super
     end
