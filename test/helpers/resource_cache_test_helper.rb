be4b04ad0907cb07fd69ea03a66240eade71e84a
Darren Worrall <darren.worrall@shopify.com>
2019-02-25 14:47:37 +0000

    Refactor kubectl sensitive output config

--- test/helpers/resource_cache_test_helper.rb
+++ test/helpers/resource_cache_test_helper.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 module ResourceCacheTestHelper
   def stub_kind_get(kind, items: [], times: 1)
-    stub_kubectl_response("get", kind, "--chunk-size=0", resp: { items: items }, kwargs: { attempts: 5 }, times: times)
+    stub_kubectl_response(
+      "get",
+      kind,
+      "--chunk-size=0",
+      resp: { items: items },
+      kwargs: { attempts: 5, output_is_sensitive: false },
+      times: times,
+    )
   end
 
   def build_resource_cache
