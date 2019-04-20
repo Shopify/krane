8188c9cc9d9a10ac828446febd4bef7942f4ff86
Brendan McGrath <brendan@redmandi.com>
2019-04-20 12:01:11 +1000

    Check volumeBindingMode in StorageClass of PVC
    If the StorageClass for a PVC has 'volumeBindingMode: WaitForFirstConsumer',
    then it won't bind until after a Pod mounts it. But the PVC must be pre-deployed,
    as the Pod requires it. So 'Pending' must be treated as a 'Success' state in this
    scenario.

--- lib/kubernetes-deploy/kubernetes_resource/persistent_volume_claim.rb
+++ lib/kubernetes-deploy/kubernetes_resource/persistent_volume_claim.rb
@@ -3,12 +3,66 @@ module KubernetesDeploy
   class PersistentVolumeClaim < KubernetesResource
     TIMEOUT = 5.minutes
 
+    def _init_storage_class(_cache)
+      @storage_class = {}
+
+      if @definition.dig("spec", "storageClassName").nil?
+        # if no storage class is defined we try to find the default one
+        # FIXME: This assumes the DefaultStorageClass admission plugin is turned on,
+        # need a way to determine this
+        is_default_class = "storageclass.beta.kubernetes.io/is-default-class"
+
+        default_sc = _cache.get_all("StorageClass").select {
+           |sc| sc.dig("metadata", "annotations", is_default_class) == "true"
+        }
+
+        if default_sc.length != 1
+          warn_msg = "Multiple default StorageClasses found. If the DefaultStorageClass " \
+            "admission plugin is turned on, all PVC creation will fail."
+          @logger.warn(warn_msg) if default_sc.length > 1
+          return
+        else
+          # using default storage class
+          sc_name = default_sc[0]["metadata"]["name"]
+        end
+      else
+        # using storage class from pvc definition
+        sc_name = @definition.dig("spec", "storageClassName")
+
+        # a storageClassName of "" is an explicit way of saying you want a PV
+        # with no defined StorageClass. We won't look up a storage_class
+        # if that is the case
+        return if sc_name == ""
+      end
+
+      @storage_class = _cache.get_instance("StorageClass", sc_name)
+
+      # check the defined StorageClass exists
+      warn_msg = "StorageClass/#{sc_name} not found. This is required for #{id} to deploy."
+      @logger.warn(warn_msg) if @storage_class.blank?
+    end
+
+    def sync(_cache)
+      super
+
+      # find the storage class (if we haven't already)
+      _init_storage_class(_cache) if @storage_class.nil?
+    end
+
     def status
       exists? ? @instance_data["status"]["phase"] : "Not Found"
     end
 
     def deploy_succeeded?
-      status == "Bound"
+      # if the StorageClass has volumeBindingMode: WaitForFirstConsumer,
+      # it won't bind until after a Pod mounts it. But it must be pre-deployed,
+      # as the Pod requires it. So 'Pending' must be treated as a 'Success' state
+
+      if @storage_class["volumeBindingMode"] == "WaitForFirstConsumer"
+        status == "Pending" || status == "Bound"
+      else
+        status == "Bound"
+      end
     end
 
     def deploy_failed?
