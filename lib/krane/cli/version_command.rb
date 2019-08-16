fa819a294316d6560f83500bb8d1231ec09daaf1
Daniel Turner <daniel.turner@shopify.com>
2019-08-16 14:52:35 -0700

    Use an include instead of monkey patching

---
+++ lib/krane/cli/version_command.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require 'active_support/concern'
+
+module Krane
+  module VersionCommand
+    extend ActiveSupport::Concern
+
+    included do
+      desc "version", "Prints the version"
+      def version
+        logger.info("Krane Version: #{KubernetesDeploy::VERSION}")
+      end
+    end
+  end
+end
