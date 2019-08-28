13d0d5687c3d1bb31579802602178baf079126b0
Daniel Turner <daniel.turner@shopify.com>
2019-08-28 10:40:55 -0700

    Add some blackbox tests

--- test/exe/krane_test.rb
+++ test/exe/krane_test.rb
@@ -7,9 +7,28 @@ class KraneTest < KubernetesDeploy::TestCase
     assert_output(nil, /Krane Version: #{KubernetesDeploy::VERSION}/) { krane.version }
   end
 
+  def test_version_success_as_black_box
+    out, err, status = krane_black_box
+    assert_predicate(status, :success?)
+    assert_empty(out)
+    assert_match(KubernetesDeploy::VERSION, err)
+  end
+
+  def test_version_failure_as_black_box
+    out, err, status = krane_black_box("-q")
+    assert_equal(status.exitstatus, 1)
+    assert_empty(out)
+    assert_match("ERROR", err)
+  end
+
   private
 
   def krane
     Krane::CLI::Krane.new
+  end
+
+  def krane_black_box(args = "")
+    path = File.expand_path("../../../exe/krane", __FILE__)
+    Open3.capture3("#{path} version #{args}")
   end
 end
