e6d533c4950718e2d714a77f2794af73730daa20
Daniel Turner <daniel.turner@shopify.com>
2019-09-10 10:18:43 -0700

    render implicit yml multi doc file correctly

--- test/integration/render_task_test.rb
+++ test/integration/render_task_test.rb
@@ -270,11 +270,11 @@ class RenderTaskTest < KubernetesDeploy::TestCase
   def test_render_only_adds_initial_doc_seperator_when_missing
     render = build_render_task(fixture_path('partials'))
     fixture = 'no-doc-seperator.yml.erb'
-    expected = "---\n# This doc has no yaml seperator\nkey1: foo\n"
+    expected = "---\n# This doc has no yaml seperator\nkey1: foo\n---\nkey2: bar\n"
 
-    assert_render_success(render.run(mock_output_stream, [fixture]))
+    assert_render_success(render.run(mock_output_stream, [fixture, fixture]))
     stdout_assertion do |output|
-      assert_equal expected, output
+      assert_equal "#{expected}#{expected}", output
     end
 
     mock_output_stream.rewind
