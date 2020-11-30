6154c9a31336d089f19da1aba5c4d0e376620f73
Tim Flapper <tim.flapper@streem.com.au>
2020-11-30 11:24:43 +1100

    Add ServiceNetworkEndpointGroup to prunable resources blacklist
    This resource, even though it comes back in discovery, is not prunable
    when running k8s cluster version 1.18.

--- lib/krane/cluster_resource_discovery.rb
+++ lib/krane/cluster_resource_discovery.rb
@@ -17,7 +17,7 @@ module Krane
     end
 
     def prunable_resources(namespaced:)
-      black_list = %w(Namespace Node ControllerRevision)
+      black_list = %w(Namespace Node ControllerRevision ServiceNetworkEndpointGroup)
       api_versions = fetch_api_versions
 
       fetch_resources(namespaced: namespaced).uniq { |r| r['kind'] }.map do |resource|
