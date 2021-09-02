4a94c8f07134823345f2fb4f28719f6d3676b145
Timothy Smith <tim.smith@shopify.com>
2021-09-02 11:27:42 -0400

    CLI tests for new restart command flags

--- lib/krane/cli/restart_command.rb
+++ lib/krane/cli/restart_command.rb
@@ -8,7 +8,7 @@ module Krane
         "deployments" => { type: :array, banner: "list of deployments",
                            desc: "List of deployment names to restart", default: [] },
         "statefulsets" => { type: :array, banner: "list of statefulsets",
-                             desc: "List of statefulset names to restart", default: [] },
+                            desc: "List of statefulset names to restart", default: [] },
         "daemonsets" => { type: :array, banner: "list of daemonsets",
                           desc: "List of daemonset names to restart", default: [] },
         "global-timeout" => { type: :string, banner: "duration", default: DEFAULT_RESTART_TIMEOUT,
