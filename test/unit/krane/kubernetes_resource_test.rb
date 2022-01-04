c077ce1e9253be4312a503ff390c522464f33ffc
Joseph Fourny <joseph.fourny@shopify.com>
2022-01-04 18:11:29 -0500

    Partial mock from `Krane::TestCase#build_runless_kubectl` overrides `client_version`

--- test/unit/krane/kubernetes_resource_test.rb
+++ test/unit/krane/kubernetes_resource_test.rb
@@ -197,8 +197,6 @@ class KubernetesResourceTest < Krane::TestCase
       .with('apply', '-f', anything, '--dry-run=client', '--output=name', anything)
       .returns(["", "", stub(success?: true)])
 
-    kubectl.expects(:client_version).returns(Gem::Version.new('1.20'))
-
     kubectl.expects(:run)
       .with('apply', '-f', anything, '--dry-run=server', '--output=name', anything)
       .returns([
@@ -213,11 +211,14 @@ class KubernetesResourceTest < Krane::TestCase
 
   def test_validate_definition_ignores_server_dry_run_unsupported_by_webhook_response_k8s_1_17
     resource = DummySensitiveResource.new
-    kubectl.expects(:run)
-      .with('apply', '-f', anything, '--dry-run=client', '--output=name', anything)
-      .returns(["", "", stub(success?: true)])
 
-    kubectl.expects(:client_version).returns(Gem::Version.new('1.17'))
+    kubectl.expects(:client_version)
+      .returns(Gem::Version.new('1.17'))
+      .at_least_once
+
+    kubectl.expects(:run)
+      .with('apply', '-f', anything, '--dry-run', '--output=name', anything)
+      .returns(["", "", stub(success?: true)])
 
     kubectl.expects(:run)
       .with('apply', '-f', anything, '--server-dry-run', '--output=name', anything)
